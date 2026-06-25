f3259e12f72af676aad223fd323a952c510bad73
Lyratm <thomaslyrasite@gmail.com>
2026-06-25 11:27:40 +0200

    Add iOS platform via Capacitor

---
+++ android/app/src/main/java/fr/v3rt/lum/MainActivity.java
@@ -0,0 +1,5 @@
+package fr.v3rt.lum;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fr.v3rt.lum',
+  appName: 'Lum',
+  webDir: 'dist'
+};
+
+export default config;
